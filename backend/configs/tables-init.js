51f37b981373c0246b4ac68616f86252ae35619d
Ibrahim <ikapadwanchwala@gmail.com>
2026-02-23 14:59:54 +0530

    Minor changes

--- backend/configs/tables-init.js
+++ backend/configs/tables-init.js
@@ -9,6 +9,7 @@ async function createTable() {
         email VARCHAR(150) NOT NULL UNIQUE,
         password VARCHAR(100) NOT NULL,
         is_active BOOLEAN DEFAULT true,
+        role ENUM("USER","ADMIN") DEFAULT "USER"
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
     `);
