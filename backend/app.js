4ff0e3f7648784cbf55f0d119d413b29447c27ec
Ibrahim <ikapadwanchwala@gmail.com>
2026-02-23 14:45:17 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors({
   origin:process.env.FRONTEND_URL,
